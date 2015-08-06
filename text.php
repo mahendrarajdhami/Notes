5a3251f61bca7b82190119f7e9acb927a0e00417
mahendra.rajdhami <mahendra@miracleinterface.com>
2015-08-06 13:36:45 +0545

    [fixed]

--- text.php
+++ text.php
@@ -1,4 +1,4 @@
 How to install yii?
 --> run following command in cmd
-path_to_php.exe path_to_yiic_from_framework path_to_project_folder
+path_to_php.exe path_to_yiic_from_framework\yiic webapp  path_to_project_folder\sample_project
 
